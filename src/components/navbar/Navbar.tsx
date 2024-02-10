966aa2b8bbf4dc453b9e5695033be19fd274b2b7
unknown <mwangiarthur777@gmail.com>
2024-02-10 12:48:30 +0300

    navbar setup

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,33 @@
 import React from "react";
 import styles from "./navbar.module.css";
+import ADMIN from "../../../public/assets/admin.jpg";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <div
-      className={`${styles.navbar} w-full shadow-md p-2 flex items-center justify-center`}
+      className={`${styles.navbar} w-full shadow-md p-2 flex items-center justify-between`}
     >
-      <div></div>
+      <div className="flex items-center">
+        <Image
+          src={ADMIN}
+          alt=""
+          width={200}
+          height={200}
+          className={styles.admin__image}
+        />
+        <div className="ml-2">
+          <h2 className="font-bold text-lg">Mosses Kiarie</h2>
+          <p className="-mt-2">Sales officer</p>
+        </div>
+        <Image
+          src="./assets/dropdown.svg"
+          alt=""
+          width={20}
+          height={20}
+          className="ml-2 cursor-pointer"
+        />
+      </div>
       <div></div>
     </div>
   );
