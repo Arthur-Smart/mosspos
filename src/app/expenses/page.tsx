c79d85056001ea8567e2d2aac01304f4e6f55aba
unknown <mwangiarthur777@gmail.com>
2024-02-11 20:15:56 +0300

    finised the expense page design

---
+++ src/app/expenses/page.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import styles from "./expenses.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import ExpenseTable from "@/components/expencetable/ExpenseTable";
+
+const page = () => {
+  return (
+    <main className="w-full p-3 mt-4">
+      <section className="flex items-center justify-between">
+        <div className="flex items-center">
+          <Link href="#">
+            <p className="font-semibold">Create new expense</p>
+          </Link>
+          <div
+            className="ml-4 flex"
+            title="Expenses total"
+          >
+            <p className="font-semibold bg-[#6C8B9F] py-[3px] px-3 rounded-lg text-white">
+              (Kes) 55,000
+            </p>
+          </div>
+        </div>
+
+        <div className="flex items-center">
+          <div className="flex">
+            <input
+              placeholder="Search order"
+              className={`${styles.search__input} outline-0`}
+            />
+            <Image
+              src="./assets/search.svg"
+              alt=""
+              width={20}
+              height={20}
+              className="ml-2"
+            />
+          </div>
+          <button className="ml-4">
+            <Image
+              src="./assets/filter.svg"
+              alt=""
+              width={27}
+              height={27}
+            />
+          </button>
+        </div>
+      </section>
+      <table className="w-full mt-7 rounded-md overflow-hidden">
+        <ExpenseTable />
+      </table>
+    </main>
+  );
+};
+
+export default page;
---
+++ src/components/expencetable/ExpenseTable.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+
+const ExpenseTable = () => {
+  return (
+    <>
+      <thead className="bg-[#E4E4E4]">
+        <tr>
+          <th className="font-semibold text-[15px] text-left p-[3px]">
+            Expense Id
+          </th>
+          <th className="font-semibold text-[15px] text-left p-[3px]">
+            Expense category
+          </th>
+          <th className="font-semibold text-[15px] text-left p-[3px]">
+            Prepared By
+          </th>
+          <th className="font-semibold text-[15px] text-left p-[3px]">
+            Amount
+          </th>
+          <th className="font-semibold text-[15px] text-left p-[3px]">Date</th>
+          <th className="font-semibold text-[15px] text-left p-[3px]">
+            Action
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td className="text-left p-[3px]">_678647...</td>
+          <td className="text-left p-[3px]">Raw material</td>
+          <td className="text-left p-[3px]">Juliet Maina</td>
+          <td className="text-left p-[3px]">8900</td>
+          <td className="text-left p-[3px]">02/02/2024</td>
+          <td className="text-left p-[3px]">...</td>
+        </tr>
+        <tr>
+          <td className="text-left p-[3px]">_678647...</td>
+          <td className="text-left p-[3px]">Raw material</td>
+          <td className="text-left p-[3px]">Juliet Maina</td>
+          <td className="text-left p-[3px]">8900</td>
+          <td className="text-left p-[3px]">02/02/2024</td>
+          <td className="text-left p-[3px]">...</td>
+        </tr>
+        <tr>
+          <td className="text-left p-[3px]">_678647...</td>
+          <td className="text-left p-[3px]">Raw material</td>
+          <td className="text-left p-[3px]">Juliet Maina</td>
+          <td className="text-left p-[3px]">8900</td>
+          <td className="text-left p-[3px]">02/02/2024</td>
+          <td className="text-left p-[3px]">...</td>
+        </tr>
+        <tr>
+          <td className="text-left p-[3px]">_678647...</td>
+          <td className="text-left p-[3px]">Raw material</td>
+          <td className="text-left p-[3px]">Juliet Maina</td>
+          <td className="text-left p-[3px]">8900</td>
+          <td className="text-left p-[3px]">02/02/2024</td>
+          <td className="text-left p-[3px]">...</td>
+        </tr>
+        <tr>
+          <td className="text-left p-[3px]">_678647...</td>
+          <td className="text-left p-[3px]">Raw material</td>
+          <td className="text-left p-[3px]">Juliet Maina</td>
+          <td className="text-left p-[3px]">8900</td>
+          <td className="text-left p-[3px]">02/02/2024</td>
+          <td className="text-left p-[3px]">...</td>
+        </tr>
+        <tr>
+          <td className="text-left p-[3px]">_678647...</td>
+          <td className="text-left p-[3px]">Raw material</td>
+          <td className="text-left p-[3px]">Juliet Maina</td>
+          <td className="text-left p-[3px]">8900</td>
+          <td className="text-left p-[3px]">02/02/2024</td>
+          <td className="text-left p-[3px]">...</td>
+        </tr>
+        <tr>
+          <td className="text-left p-[3px]">_678647...</td>
+          <td className="text-left p-[3px]">Raw material</td>
+          <td className="text-left p-[3px]">Juliet Maina</td>
+          <td className="text-left p-[3px]">8900</td>
+          <td className="text-left p-[3px]">02/02/2024</td>
+          <td className="text-left p-[3px]">...</td>
+        </tr>
+        <tr>
+          <td className="text-left p-[3px]">_678647...</td>
+          <td className="text-left p-[3px]">Raw material</td>
+          <td className="text-left p-[3px]">Juliet Maina</td>
+          <td className="text-left p-[3px]">8900</td>
+          <td className="text-left p-[3px]">02/02/2024</td>
+          <td className="text-left p-[3px]">...</td>
+        </tr>
+        <tr>
+          <td className="text-left p-[3px]">_678647...</td>
+          <td className="text-left p-[3px]">Raw material</td>
+          <td className="text-left p-[3px]">Juliet Maina</td>
+          <td className="text-left p-[3px]">8900</td>
+          <td className="text-left p-[3px]">02/02/2024</td>
+          <td className="text-left p-[3px]">...</td>
+        </tr>
+      </tbody>
+    </>
+  );
+};
+
+export default ExpenseTable;
