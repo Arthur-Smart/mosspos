58520db6969ad0bac7f817f3517539bbff32b704
unknown <mwangiarthur777@gmail.com>
2024-02-11 19:58:02 +0300

    designed the Sales page

---
+++ src/app/sales/page.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styles from "./sales.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import SalesTable from "@/components/salestable/SalesTable";
+
+const page = () => {
+  return (
+    <main className="w-full p-3 mt-4">
+      <section className="flex items-center justify-between">
+        <Link href="#">
+          <p className="font-semibold">Create new order</p>
+        </Link>
+        <div className="flex items-center">
+          <div className="flex">
+            <input
+              placeholder="Search order"
+              className={`${styles.search__input} outline-0`}
+            />
+            <Image
+              src="./assets/search.svg"
+              alt=""
+              width={20}
+              height={20}
+              className="ml-2"
+            />
+          </div>
+          <button className="ml-4">
+            <Image
+              src="./assets/filter.svg"
+              alt=""
+              width={27}
+              height={27}
+            />
+          </button>
+        </div>
+      </section>
+      <table className="w-full mt-7 rounded-md overflow-hidden">
+        <SalesTable />
+      </table>
+    </main>
+  );
+};
+
+export default page;
---
+++ src/components/salestable/SalesTable.tsx
@@ -0,0 +1,123 @@
+import React from "react";
+
+const SalesTable = () => {
+  return (
+    <>
+      <thead className="bg-[#E4E4E4]">
+        <tr>
+          <th className="font-semibold text-[15px] text-left p-[3px]">
+            Product
+          </th>
+          <th className="font-semibold text-[15px] text-left p-[3px]">
+            Customer
+          </th>
+          <th className="font-semibold text-[15px] text-left p-[3px]">
+            Cost(kes)
+          </th>
+          <th className="font-semibold text-[15px] text-left p-[3px]">
+            Quantity
+          </th>
+          <th className="font-semibold text-[15px] text-left p-[3px]">Total</th>
+          <th className="font-semibold text-[15px] text-left p-[3px]">Date</th>
+          <th className="font-semibold text-[15px] text-left p-[3px]">
+            Action
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td className="text-left p-[3px]">Skim Coat</td>
+          <td className="text-left p-[3px]">Joel maina</td>
+          <td className="text-left p-[3px]">5768</td>
+          <td className="text-left p-[3px]">55</td>
+          <td className="text-left p-[3px]">56000</td>
+          <td className="text-left p-[3px]">15/01/2024</td>
+          <td className="text-left p-[3px]">...</td>
+        </tr>
+        <tr>
+          <td className="text-left p-[3px]">Skim Coat</td>
+          <td className="text-left p-[3px]">Joel maina</td>
+          <td className="text-left p-[3px]">5768</td>
+          <td className="text-left p-[3px]">55</td>
+          <td className="text-left p-[3px]">56000</td>
+          <td className="text-left p-[3px]">15/01/2024</td>
+          <td className="text-left p-[3px]">...</td>
+        </tr>
+        <tr>
+          <td className="text-left p-[3px]">Skim Coat</td>
+          <td className="text-left p-[3px]">Joel maina</td>
+          <td className="text-left p-[3px]">5768</td>
+          <td className="text-left p-[3px]">55</td>
+          <td className="text-left p-[3px]">56000</td>
+          <td className="text-left p-[3px]">15/01/2024</td>
+          <td className="text-left p-[3px]">...</td>
+        </tr>
+        <tr>
+          <td className="text-left p-[3px]">Skim Coat</td>
+          <td className="text-left p-[3px]">Joel maina</td>
+          <td className="text-left p-[3px]">5768</td>
+          <td className="text-left p-[3px]">55</td>
+          <td className="text-left p-[3px]">56000</td>
+          <td className="text-left p-[3px]">15/01/2024</td>
+          <td className="text-left p-[3px]">...</td>
+        </tr>
+        <tr>
+          <td className="text-left p-[3px]">Skim Coat</td>
+          <td className="text-left p-[3px]">Joel maina</td>
+          <td className="text-left p-[3px]">5768</td>
+          <td className="text-left p-[3px]">55</td>
+          <td className="text-left p-[3px]">56000</td>
+          <td className="text-left p-[3px]">15/01/2024</td>
+          <td className="text-left p-[3px]">...</td>
+        </tr>
+        <tr>
+          <td className="text-left p-[3px]">Skim Coat</td>
+          <td className="text-left p-[3px]">Joel maina</td>
+          <td className="text-left p-[3px]">5768</td>
+          <td className="text-left p-[3px]">55</td>
+          <td className="text-left p-[3px]">56000</td>
+          <td className="text-left p-[3px]">15/01/2024</td>
+          <td className="text-left p-[3px]">...</td>
+        </tr>
+        <tr>
+          <td className="text-left p-[3px]">Skim Coat</td>
+          <td className="text-left p-[3px]">Joel maina</td>
+          <td className="text-left p-[3px]">5768</td>
+          <td className="text-left p-[3px]">55</td>
+          <td className="text-left p-[3px]">56000</td>
+          <td className="text-left p-[3px]">15/01/2024</td>
+          <td className="text-left p-[3px]">...</td>
+        </tr>
+        <tr>
+          <td className="text-left p-[3px]">Skim Coat</td>
+          <td className="text-left p-[3px]">Joel maina</td>
+          <td className="text-left p-[3px]">5768</td>
+          <td className="text-left p-[3px]">55</td>
+          <td className="text-left p-[3px]">56000</td>
+          <td className="text-left p-[3px]">15/01/2024</td>
+          <td className="text-left p-[3px]">...</td>
+        </tr>
+        <tr>
+          <td className="text-left p-[3px]">Skim Coat</td>
+          <td className="text-left p-[3px]">Joel maina</td>
+          <td className="text-left p-[3px]">5768</td>
+          <td className="text-left p-[3px]">55</td>
+          <td className="text-left p-[3px]">56000</td>
+          <td className="text-left p-[3px]">15/01/2024</td>
+          <td className="text-left p-[3px]">...</td>
+        </tr>
+        <tr>
+          <td className="text-left p-[3px]">Skim Coat</td>
+          <td className="text-left p-[3px]">Joel maina</td>
+          <td className="text-left p-[3px]">5768</td>
+          <td className="text-left p-[3px]">55</td>
+          <td className="text-left p-[3px]">56000</td>
+          <td className="text-left p-[3px]">15/01/2024</td>
+          <td className="text-left p-[3px]">...</td>
+        </tr>
+      </tbody>
+    </>
+  );
+};
+
+export default SalesTable;
