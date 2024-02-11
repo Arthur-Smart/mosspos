317d1b196c00879dccd4f158defefc12f4987c07
unknown <mwangiarthur777@gmail.com>
2024-02-11 10:22:36 +0300

    add product page

--- src/app/newcustomer/page.tsx
+++ src/app/newcustomer/page.tsx
@@ -23,7 +23,7 @@ const page = () => {
               height={35}
             />
           </div>
-          <button className="bg-[#283593] w-[200px] mt-3 flex items-center justify-center rounded-md py-2 ">
+          <button className="bg-[#283593] w-[230px] mt-3 flex items-center justify-center rounded-md py-2 ">
             <Image
               src="./assets/add.svg"
               alt=""
@@ -37,7 +37,79 @@ const page = () => {
             Allowed files; jpg, jpeg, png
           </p>
         </div>
-        <div className="w-[75%]"></div>
+        <div className="w-[75%]">
+          <div className="w-full">
+            <div className="flex w-full items-center justify-between">
+              <div className={`${styles.input} flex flex-col`}>
+                <label className="font-semibold">Product Name*</label>
+                <input
+                  placeholder="Enter product name"
+                  type="text"
+                  className="border-[1px] border-gray-200 outline-0 py-2 px-3 rounded-md"
+                />
+              </div>
+              <div className={`${styles.input} flex flex-col`}>
+                <label className="font-semibold">SKU*</label>
+                <input
+                  placeholder="Enter product name"
+                  type="text"
+                  className="border-[1px] border-gray-200 outline-0 py-2 px-3 rounded-md"
+                />
+              </div>
+              <div className={`${styles.input} flex flex-col `}>
+                <label className="font-semibold">Barcode type*</label>
+                <input
+                  placeholder="Enter product name"
+                  type="text"
+                  className="border-[1px] border-gray-200 outline-0 py-2 px-3 rounded-md"
+                />
+              </div>
+            </div>
+            <div className="flex w-full items-center justify-between mt-7">
+              <div className={`${styles.input} flex flex-col`}>
+                <label className="font-semibold">Unit*</label>
+                <input
+                  placeholder="Enter number of units"
+                  type="text"
+                  className="border-[1px] border-gray-200 outline-0 py-2 px-3 rounded-md"
+                />
+              </div>
+              <div className={`${styles.input} flex flex-col`}>
+                <label className="font-semibold">Cost*</label>
+                <input
+                  placeholder="Enter cost"
+                  type="text"
+                  className="border-[1px] border-gray-200 outline-0 py-2 px-3 rounded-md"
+                />
+              </div>
+              <div className={`${styles.input} flex flex-col `}>
+                <label className="font-semibold">Category*</label>
+                <select className="border-[1px] border-gray-200 outline-0 py-2 px-3 rounded-md">
+                  <option value="interior">Interior</option>
+                  <option value="enterior">Enterior</option>
+                </select>
+              </div>
+            </div>
+            <div className="flex w-full items-center justify-between mt-7">
+              <div className={`${styles.input} flex flex-col`}>
+                <label className="font-semibold">Unit*</label>
+                <input
+                  placeholder="Enter number of units"
+                  type="text"
+                  className="border-[1px] border-gray-200 outline-0 py-2 px-3 rounded-md"
+                />
+              </div>
+              <div className={`${styles.input} flex flex-col`}>
+                <label className="font-semibold">Cost*</label>
+                <input
+                  placeholder="Enter cost"
+                  type="text"
+                  className="border-[1px] border-gray-200 outline-0 py-2 px-3 rounded-md"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </main>
   );
