a91a6909831cfcdb2efed7e503d6ef92403b23b5
unknown <mwangiarthur777@gmail.com>
2024-02-13 09:20:05 +0300

    designined the create  invoice page

--- src/app/new-invoice/page.tsx
+++ src/app/new-invoice/page.tsx
@@ -1,8 +1,89 @@
 import React from "react";
 import styles from "./newinvoice.module.css";
+import Image from "next/image";
+import Order from "@/components/order/Order";
 
 const page = () => {
-  return <main></main>;
+  return (
+    <main className="w-full p-3 mt-4">
+      <section className="w-full flex flex-col">
+        <div className="flex flex-col">
+          <label className="font-semibold ">Customer name*</label>
+          <div className="flex items-center justify-between border-[1px] border-gray-200 outline-0 py-2 px-3 rounded-md">
+            <input
+              type="text"
+              placeholder="Enter customer name "
+              className="w-[80%] outline-0"
+            />
+            <Image
+              src="./assets/search.svg"
+              width={25}
+              height={25}
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="flex flex-col mt-4">
+          <label className="font-semibold ">Prepared By*</label>
+          <div className="flex items-center justify-between border-[1px] border-gray-200 outline-0 py-2 px-3 rounded-md">
+            <input
+              type="text"
+              placeholder="Enter person's name "
+              className="w-[80%] outline-0"
+            />
+            <Image
+              src="./assets/search.svg"
+              width={25}
+              height={25}
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="flex flex-col mt-4 w-full">
+          <div className="w-full flex items-center gap-2">
+            <div className="w-[40%]">
+              <label className="font-semibold ">Product</label>
+              <div className="flex items-center justify-between  border-[1px] border-gray-200 outline-0 py-2 px-3 rounded-md">
+                <input
+                  type="text"
+                  placeholder="Enter product name "
+                  className=" outline-0 w-[80%]"
+                />
+                <Image
+                  src="./assets/search.svg"
+                  width={25}
+                  height={25}
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="w-[40%] ">
+              <label className="font-semibold ">Quantity</label>
+              <div className="flex items-center justify-between border-[1px] border-gray-200 outline-0 py-2 px-3 rounded-md">
+                <input
+                  type="text"
+                  placeholder="Quantity purchased"
+                  className=" outline-0 w-[100%]"
+                />
+              </div>
+            </div>
+            <button className="w-[18%] bg-[#283593] py-2 px-3 rounded-md text-white mt-5">
+              Add
+            </button>
+          </div>
+          <div className="w-full flex flex-wrap  gap-2 border-[1px] border-gray-200 outline-0 py-2 px-3 rounded-md mt-2">
+            <Order />
+            <Order />
+            <Order />
+            <Order />
+            <Order />
+            <Order />
+            <Order />
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default page;
---
+++ src/components/order/Order.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./order.module.css";
+import Image from "next/image";
+
+const Order = () => {
+  return (
+    <div className="py-2 px-3 rounded-full flex items-start border-[1px] border-gray-200">
+      <div>
+        <h1 className="font-semibold">Silk Vinly</h1>
+        <p className="text-[#6E7373] text-[14px]">Qty 3 | Cost 3,000</p>
+      </div>
+      <Image
+        src="./assets/close.svg"
+        alt=""
+        width={15}
+        height={15}
+        className="ml-3 cursor-pointer"
+      />
+    </div>
+  );
+};
+
+export default Order;
